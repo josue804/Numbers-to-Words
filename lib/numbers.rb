3fe406949b6afb039b45680a1503e104ba146cfd
Yelena Nedelko & Josue Valverde <student@epicodus.com>
2015-08-04 14:54:42 -0700

    Passed spec: returns correct String for numbers 13-19.

--- lib/numbers.rb
+++ lib/numbers.rb
@@ -1,6 +1,10 @@
 class Fixnum
   define_method(:numbers) do
     base_number = { 1 =>"one", 2 =>"two", 3 =>"three", 4 =>"four", 5 =>"five", 6 =>"six", 7 =>"seven", 8 =>"eight", 9 =>"nine", 10 =>"ten", 11 =>"eleven", 12 =>"twelve"}
-    base_number.fetch(self)
+    if self <= 12
+      base_number.fetch(self)
+    elsif self <= 19 && self >= 13
+      base_number.fetch(self-10).concat("teen")
+    end
   end
 end
--- spec/numbers_spec.rb
+++ spec/numbers_spec.rb
@@ -5,4 +5,7 @@ describe('Fixnum#numbers') do
   it("When given a number 1-12, it returns the value as a String.") do
     expect((11).numbers()).to(eq("eleven"))
   end
+  it("returns correct String for numbers 13-19") do
+    expect((17).numbers()).to(eq("seventeen"))
+  end
 end
