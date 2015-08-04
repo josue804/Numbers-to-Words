f464dfa4546d5c91a3aed15fe9ff57a1b1250ebe
Yelena Nedelko & Josue Valverde <student@epicodus.com>
2015-08-04 15:21:34 -0700

    Finished 3rd spec: returns correct String for numbers 20 - 99 and fixed 'thirteen' and 'fifteen'

--- lib/numbers.rb
+++ lib/numbers.rb
@@ -1,10 +1,19 @@
 class Fixnum
   define_method(:numbers) do
     base_number = { 1 =>"one", 2 =>"two", 3 =>"three", 4 =>"four", 5 =>"five", 6 =>"six", 7 =>"seven", 8 =>"eight", 9 =>"nine", 10 =>"ten", 11 =>"eleven", 12 =>"twelve"}
+    tens_numbers = {20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
     if self <= 12
       base_number.fetch(self)
-    elsif self <= 19 && self >= 13
-      base_number.fetch(self-10).concat("teen")
+    elsif self <= 19
+      if self == 13
+        "thirteen"
+      elsif self == 15
+        "fifteen"
+      else
+        base_number.fetch(self-10).concat("teen")
+      end
+    elsif self < 100
+      tens_numbers.fetch(self - self%10).concat(base_number.fetch(self%10))
     end
   end
 end
--- spec/numbers_spec.rb
+++ spec/numbers_spec.rb
@@ -3,9 +3,12 @@ require ('numbers')
 
 describe('Fixnum#numbers') do
   it("When given a number 1-12, it returns the value as a String.") do
-    expect((11).numbers()).to(eq("eleven"))
+    expect((13).numbers()).to(eq("thirteen"))
   end
   it("returns correct String for numbers 13-19") do
     expect((17).numbers()).to(eq("seventeen"))
+  end
+  it("returns correct String for numbers 20-99") do
+    expect((88).numbers()).to(eq("eightyeight"))
   end
 end
